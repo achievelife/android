89c5dd5bbc46f2d3b09056b96ae87694b997a835
Jonathan Strzalka <Jonathan Strzalka>
2016-10-06 15:15:49 -0400

    added some more activity log functionality in xml

--- AchieveLife/app/src/main/java/cse442/achievelife/activity_log.java
+++ AchieveLife/app/src/main/java/cse442/achievelife/activity_log.java
@@ -4,6 +4,15 @@ package cse442.achievelife;
  * Created by jrser on 9/29/2016.
  */
 
-public class activity_log {
-    // TODO FOR SPRINT 2
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class activity_log extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_log);
+    }
 }

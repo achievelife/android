f4e710376044cfe42b49f7dad6db2a80c22a777f
ywang77 <ywang77@buffalo.edu>
2016-10-08 17:55:04 -0400

    oldtasks

--- app/src/test/java/edu/ywang77buffalo/achievelife/ExampleUnitTest.java
+++ app/src/test/java/edu/ywang77buffalo/achievelife/ExampleUnitTest.java
@@ -1,13 +1,11 @@
-package cse442.achievelife;
+package edu.ywang77buffalo.achievelife;
 
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
     @Test

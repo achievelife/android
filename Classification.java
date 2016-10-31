f86cc2eb8354be0d847525fbdae3af3d762d5862
fyang23 <fyang23@buffalo.edu>
2016-10-31 02:57:52 -0400

    Add files via upload

---
+++ Classification.java
@@ -0,0 +1,18 @@
+package com.example.english.app;
+
+/**
+ * Created by English on 2016/10/27.
+ */
+
+public class Classification {
+    int i1;
+    public Classification(){
+
+    }
+    public void setInt(int i){
+        i1=i;
+    }
+    public int getInt(){
+        return i1;
+    }
+}

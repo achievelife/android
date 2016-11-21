d01b3cd316544348b365931589293edbeeecd58e
mordecai1234 <jessetay@buffalo.edu>
2016-11-20 21:36:27 -0500

    Facebook Login Sprint 4

---
+++ app/src/main/java/cse442/achievelife/MyApplication.java
@@ -0,0 +1,40 @@
+package cse442.achievelife;
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by Admin on 10/25/2016.
+ */
+public class MyApplication extends Application{
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        printHashKey();
+    }
+
+    public void printHashKey(){
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    getApplicationContext().getPackageName(),
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("HashKey:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+    }
+}

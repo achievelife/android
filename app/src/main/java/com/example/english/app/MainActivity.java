36b65b22ba5ab70b8729d016142b4827bda71a15
fyang23 <fyang23@buffalo.edu>
2016-11-20 22:19:49 -0500

    side bar

---
+++ app/src/main/java/com/example/english/app/MainActivity.java
@@ -0,0 +1,227 @@
+package com.example.english.app;
+
+import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MainActivity extends AppCompatActivity
+        implements OnMapReadyCallback,NavigationView.OnNavigationItemSelectedListener {
+
+    private static final long MIN_DISTANCE_CHANG_FOR_UPDATE = 10;
+    private static final long MINI_TIME_BW_UPDATE = 1000 * 60 * 1;
+    private GoogleMap mMap;
+    private SupportMapFragment supportMapFragment;
+    private Marker marker;
+    private Location currentLocation;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        supportMapFragment=(SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        supportMapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng p = new LatLng(43.000582, -78.781136);
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(p,13));
+
+     /* Location currentLocation = new Location("");
+        currentLocation.setLatitude(43.000774);
+        currentLocation.setLongitude(-78.7911897);*/
+        /*mMap.setOnInfoWindowClickListener((GoogleMap.OnInfoWindowClickListener) this);*/
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.setting) {
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item){
+            // Handle navigation view item clicks here.
+
+            int id = item.getItemId();
+            if (id == R.id.Center_for_the_Arts) {
+                mark(1);
+            } else if (id == R.id.Lockwood_Memorial_Library) {
+                mark(2);
+            } else if (id == R.id.Student_Union) {
+                mark(3);
+            } else if (id == R.id.Clemens_Hall) {
+                mark(4);
+            } else if (id == R.id.Charles_B_Sears_Law_Library) {
+                mark(5);
+            } else if (id == R.id.Alumni_Arena) {
+                mark(6);
+            } else if (id == R.id.Level_Bound) {
+
+            } else if (id == R.id.Activity_Log) {
+                startActivity(new Intent(MainActivity.this, basicActivity.class));
+            }else if (id == R.id.Setting) {
+
+            }else if (id == R.id.Intelligence) {
+            }else if (id == R.id.Fitness) {
+            }else if (id == R.id.Society) {
+            }
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+        }
+
+   public void mark(int i){
+       switch(i){
+           case 1:
+               LatLng p1 = new LatLng(43.001205, -78.783030);
+               mMap.addMarker(new MarkerOptions().position(p1).title("UB Center for the Arts"));
+               mMap.moveCamera(CameraUpdateFactory.newLatLng(p1));
+               MarkerLocationZoom(43.001205, -78.783030, 18);
+               /*setMarkers(currentLocation, "UB Center for the Arts", 43.001205, -78.783030);*/
+               break;
+
+           case 2:
+                  LatLng p2 = new LatLng(43.000407, -78.785796);
+                  mMap.addMarker(new MarkerOptions().position(p2).title("Lockwood Memorial Library"));
+                  mMap.moveCamera(CameraUpdateFactory.newLatLng(p2));
+                  MarkerLocationZoom(43.000407, -78.785796, 18);
+
+               break;
+           case 3:
+               LatLng p3 = new LatLng(43.001256, -78.786241);
+               mMap.addMarker(new MarkerOptions().position(p3).title("Student Union"));
+               mMap.moveCamera(CameraUpdateFactory.newLatLng(p3));
+               MarkerLocationZoom(43.001256, -78.786241, 18);
+
+               break;
+           case 4:
+                 LatLng p4 = new LatLng(43.000523, -78.784979);
+                 mMap.addMarker(new MarkerOptions().position(p4).title("Clemens Hall"));
+                 mMap.moveCamera(CameraUpdateFactory.newLatLng(p4));
+                 MarkerLocationZoom(43.000523, -78.784979, 18);
+
+               break;
+           case 5:
+                LatLng p5 = new LatLng(43.000412, -78.787968);
+                mMap.addMarker(new MarkerOptions().position(p5).title("Charles B. Sears Law Library"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(p5));
+               MarkerLocationZoom(43.000412, -78.787968, 18);
+
+               break;
+           case 6:
+               LatLng p6 = new LatLng(43.000582, -78.781136);
+               mMap.addMarker(new MarkerOptions().position(p6).title("Alumni Arena"));
+               mMap.moveCamera(CameraUpdateFactory.newLatLng(p6));
+               MarkerLocationZoom(43.000582, -78.781136, 18);
+
+               break;
+
+       }
+   }
+    public void MarkerLocationZoom(double latitude, double longitude, float zoom) {
+        // Add a marker in UB bookstore and move the camera
+        LatLng UBBookStore = new LatLng(latitude, longitude);
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(UBBookStore, zoom);
+        //googleMap.moveCamera(CameraUpdateFactory.newLatLng(UBBookStore));
+        mMap.moveCamera(cameraUpdate);
+
+    }
+
+
+   public void setMarkers(Location currentLocation, String locality, double latitude, double longitude) {
+
+        Location destination = new Location(locality);
+        destination.setLatitude(latitude);
+        destination.setLongitude(longitude);
+
+        float distance = currentLocation.distanceTo(destination);  /** get the distance*******************************/
+
+        if (distance < 100) {
+            marker = mMap.addMarker(new MarkerOptions().title(locality)
+                    .position(new LatLng(latitude, longitude))
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                    .snippet("The distance is: " + Float.toString(distance) + " meters"));
+        } else if (distance < 500) {
+            marker = mMap.addMarker(new MarkerOptions().title(locality)
+                    .position(new LatLng(latitude, longitude))
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                    .snippet("The distance is: " + Float.toString(distance) + " meters"));
+        } else {
+            marker = mMap.addMarker(new MarkerOptions().title(locality)
+                    .position(new LatLng(latitude, longitude))
+                    .snippet("The distance is: " + Float.toString(distance) + " meters"));
+        }
+        marker.showInfoWindow();
+    }
+
+}
+
+
+
+
+
+

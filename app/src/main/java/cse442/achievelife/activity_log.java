a026152810fcf54f815a10b61e8b6ec20ed0af94
Jonathan Strzalka <Jonathan Strzalka>
2016-11-21 00:08:43 -0500

    Fixed some bugs created from the merge

--- app/src/main/java/cse442/achievelife/activity_log.java
+++ app/src/main/java/cse442/achievelife/activity_log.java
@@ -4,12 +4,23 @@ package cse442.achievelife;
  * Created by jrser on 9/29/2016.
  */
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class activity_log extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+//import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class activity_log extends AppCompatActivity {
 
@@ -23,5 +34,13 @@ public class activity_log extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_log);
+
+        textView1 = (TextView)findViewById(R.id.textView1);
+        textView1.setText("ACTIVITY LOG");
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, mobileArray);
+
+        listView1 = (ListView) findViewById(R.id.mobile_list);
+        listView1.setAdapter(adapter);
     }
 }
